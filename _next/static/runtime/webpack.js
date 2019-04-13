3de70ff829b83c294b8590b786f7ac75117bc52f
Matheus Barros <matheuslbarros@gmail.com>
2019-04-13 19:12:05 -0300

    Change profile url

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "624993d74a6b9fcea905";
+/******/ 	var hotCurrentHash = "365cfb99bbc520926b24";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
